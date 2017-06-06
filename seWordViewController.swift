a6f76f6f9ecc638a4953b9a6ea49b74b193add9b
Yosuke <yosukeyosuke2002@gmail.com>
2017-06-06 21:33:26 +0900

    6/6、主にテスト目的

---
+++ seWordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  seWordViewController.swift
+//  Kaijosai2017
+//
+//  Created by Yosuke Onuma on 2017/06/06.
+//  Copyright © 2017年 bunjitsu.programming. All rights reserved.
+//
+
+import UIKit
+
+class seWordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
