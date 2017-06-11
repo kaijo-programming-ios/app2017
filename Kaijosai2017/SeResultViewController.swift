04cfd9aeda7c5d39f24b06f040b5f99e914ef674
Yosuke <yosukeyosuke2002@gmail.com>
2017-06-11 19:11:10 +0900

    テーブル日表示バグ
    検索機能をつけると、なぜかテーブルが表示されないので、新たなブランチで作成する

--- Kaijosai2017/SeResultViewController.swift
+++ Kaijosai2017/SeResultViewController.swift
@@ -8,29 +8,51 @@
 
 import UIKit
 
-class SeResultViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SeResultViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
-    @IBOutlet weak var table: UITableView!
     
-    let imgArray: NSArray = ["kaijopic","mappic","navipic","searchpic","trainpic","wordpic"]
-    let eventArray: NSArray = ["海城説明会","校内案内","ハーイ！ナビターイム！","ご注文はこの企画ですか？","The 鉄研","中高生が全力で〇〇やって見た！"]
-    let organArray: NSArray = ["海原会","文実ツアー部","高１有志","2年4組","鉄道研究部","出版部"]
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var keywordSearchBar: UISearchBar!
+    
+    //企画情報
+    let imgArray = ["kaijopic","mappic","navipic","searchpic","trainpic","wordpic"]
+    let eventArray = ["海城説明会","校内案内","ハーイ！ナビターイム！","ご注文はこの企画ですか？","The 鉄研","中高生が全力で〇〇やって見た！"]
+    let organArray = ["海原会","文実ツアー部","高１有志","2年4組","鉄道研究部","出版部"]
+    
+    //検索結果の配列
+    var searchResult1 = [String]()
+    var searchResult2 = [String]()
+    var searchResult3 = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        //デリゲート先の設定
+        self.table.delegate = self
+        self.table.dataSource = self
+        keywordSearchBar.delegate = self
+        
+        //何も入力されていなくても検索キーを押せるようにする
+        keywordSearchBar.enablesReturnKeyAutomatically = false
+        
+        //検索結果配列にデータをコピー
+        searchResult1 = imgArray
+        searchResult2 = eventArray
+        searchResult3 = organArray
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imgArray.count
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.table.reloadData()
     }
+    
+   
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         //インスタンス生成
         let cell = table.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
-        let img = UIImage(named:"\(imgArray[indexPath.row])")
+        let img = UIImage(named:"\(searchResult1[indexPath.row])")
         
         //Tag1生成
         let imageView = table.viewWithTag(1) as! UIImageView
@@ -38,13 +60,17 @@ class SeResultViewController: UIViewController, UITableViewDataSource, UITableVi
         
         //Tag2生成
         let label1 = table.viewWithTag(2) as! UILabel
-        label1.text = "\(eventArray[indexPath.row])"
+        label1.text = "\(searchResult2[indexPath.row])"
         
         //Tag3の生成
         let label2 = table.viewWithTag(3) as! UILabel
-        label2.text = "\(organArray[indexPath.row])"
+        label2.text = "\(searchResult3[indexPath.row])"
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResult2.count
     }
     
     override func didReceiveMemoryWarning() {
@@ -52,7 +78,41 @@ class SeResultViewController: UIViewController, UITableViewDataSource, UITableVi
         // Dispose of any resources that can be recreated.
     }
     
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        keywordSearchBar.endEditing(true)
+        
+        //カウンター変数
+        var counter = -1
+        
+        //一旦検索結果をリセット
+        searchResult1.removeAll()
+        searchResult2.removeAll()
+        searchResult3.removeAll()
+        
+        if(keywordSearchBar.text == "") {
+            //空の時は全表示
+            searchResult1 = imgArray
+            searchResult2 = eventArray
+            searchResult3 = organArray
+        } else {
+            for data in eventArray {
+                counter += 1
+                if data.contains(keywordSearchBar.text!) {
+                    //絞ったデータを代入
+                    searchResult2.append(data)
+                    searchResult1.append(imgArray[counter])
+                    searchResult3.append(organArray[counter])
+                }
+            }
+        }
+        self.table.reloadData()
+        
+    }
+    
     /*
     // MARK: - Navigation
 
