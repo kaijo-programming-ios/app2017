44f9cd3f28fd5a6c84aed9c5ffabab6394c6fc11
大沼陽介 <onumayosuke@oonumayousuke.local>
2017-08-01 19:51:29 +0900

    改善　ブックマーク機能（途中）
    リストからの、画面遷移機能を追加。

--- Kaijosai2017/bookmarkTableViewController.swift
+++ Kaijosai2017/bookmarkTableViewController.swift
@@ -146,6 +146,26 @@ class bookmarkTableViewController: UITableViewController {
         }
     }
     
+    // Cell が選択された時の処理
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        // セルの選択解除
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // 指定した ID の Segue を初期化
+        self.performSegue(withIdentifier: "showDetail2", sender: bookmarkList[(indexPath as NSIndexPath).row].components(separatedBy: ","))
+    }
+    
+    // セグエで詳細ページに移動する際のデータの受け渡し
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        // セグエが showDetail の時の処理
+        if segue.identifier == "showDetail2"
+        {
+            // 移動先のビューコントローラの data プロパティに値を設定する
+            (segue.destination as! eventDetailViewController).data = sender as! [String]
+        }
+    }
     
     
 }
